606691152090bf571490d535fdad8c6b6be702c1
gleonag <gleonag@gmail.com>
2022-10-27 19:47:52 +0000

    starting exercise

--- src/js/component/Navbar.jsx
+++ src/js/component/Navbar.jsx
@@ -2,8 +2,21 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav>
-        Soy el navbar
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <div class="col-2 justify-content-end">
+                    <div class="btn-group">
+                        <button type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                            <i class="bi bi-nut"></i>
+                          Favourites
+                        </button>
+                        <ul class="dropdown-menu">
+                   
+                        </ul>
+                    </div>
+          </div>
+      </div>
     </nav>
   )
 }
