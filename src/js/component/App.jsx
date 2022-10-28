6ee4ac0a73d69209453a31997d12a6116be1f0b4
gleonag <gleonag@gmail.com>
2022-10-28 23:47:06 +0000

    props

--- src/js/component/App.jsx
+++ src/js/component/App.jsx
@@ -5,19 +5,19 @@ import Navbar from "./Navbar.jsx";
 import Footer from "./Footer.jsx";
 import Home from "../views/Home.jsx";
 import View from "../views/View.jsx";
-
-
+import Card from "./Card.jsx";
 
 //create your first component
 const App = () => {
+
+
 	return (
 		<div className="text-center">
 			<Navbar/>
+			<Card/>
 			<BrowserRouter>
-			
 				<Route path="/" component={Home} />
-				<Route path="/detail/:id" component={View} />
-			
+				<Route path="/detail/:id" component={View}/>			
 			</BrowserRouter>
 			<Footer/>
 		
